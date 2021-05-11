26f870957c2bff06675df9ca83ada460ec402ee7
wanghao <675072584@qq.com>
2021-05-11 20:49:31 +0800

    [modified] C++ points

---
+++ Linux/README.md
@@ -0,0 +1,7 @@
+# grep、awk、sed
+
+# linux 下动态库搜索顺序
+
+# gdb 调试
+
+# vim 搜索、替换
---
+++ 数据结构/quickSort.cpp
@@ -0,0 +1,14 @@
+/*
+快速排序：每次选择一个元素并且将整个数组以选择的元素为基点分成两个部分。小于该元素的放在左边，大于该元素的放在右边；
+时间复杂度：O（NlogN）
+空间复杂度：O（logN）
+*/
+
+//代码实现
+void quickSort(int arr[], int n){
+    qSort(arr, 0, n-1);
+}
+
+void qSort(int arr[], int low, int high){
+    
+}
