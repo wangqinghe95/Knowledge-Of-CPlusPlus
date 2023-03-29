01462f9af3a69deb2aed3b15ad12ac1a005abb9c
王清河 <675072584@qq.com>
2023-03-29 14:31:12 +0800

    [add][code][ext] add code and context for decorate

--- 设计模式/Decorator.cpp
+++ 设计模式/Decorator.cpp
@@ -87,3 +87,116 @@ int main()
 
     return 0;
 }
+
+/**
+ * @brief 
+ * Component: Car
+ * ConcreteComponent：TOYOYO、VOLOVL
+ * Decorator：Function
+ * ConcreteDecorator: Navigator、AutoDrive、SpeechControl
+ */
+#include<iostream>
+#include<string>
+using namespace std;
+
+// abstract component class
+class Car
+{
+public:
+    virtual void showInfo() = 0;
+};
+
+
+// concrete component class 
+class TOYOTO : public Car
+{
+public:
+    TOYOTO(){}
+    TOYOTO(string name) {
+        m_name = name;
+    }
+    void showInfo(){
+        cout << m_name << endl;
+    }
+private:
+    string m_name;
+};
+
+class VOLOVL : public Car
+{
+public:
+    VOLOVL(){}
+    VOLOVL(string name) {
+        m_name = name;
+    }
+    void showInfo(){
+        cout << m_name << endl;
+    }
+private:
+    string m_name;
+};
+
+// abstract decorator
+class Function : public Car
+{
+protected:
+    Car* m_pCar;
+public:
+    void Decorate(Car* pCar){
+        m_pCar = pCar;
+    }
+    void showInfo(){
+        if(m_pCar != NULL){
+            m_pCar->showInfo();
+        }
+    }
+};
+
+// concrete decorator
+class Navigator : public Function
+{
+public:
+    void showInfo(){
+        cout << "Navigator: ";
+        Function::showInfo();
+    }
+};
+
+// automatic drive
+class AutoDrive : public Function
+{
+public:
+    void showInfo(){
+        cout << "AutoDrive: ";
+        Function::showInfo();
+    }
+};
+
+// speech control
+class SpeechControl : public Function
+{
+public:
+    void showInfo(){
+        cout << "SpeechControl: ";
+        Function::showInfo();
+    }
+};
+
+int main()
+{
+    Car* p_CToyota = new TOYOTO("toyoto");
+    Navigator* p_CNavi = new Navigator();
+    AutoDrive* p_CAuto = new AutoDrive();
+    p_CNavi->Decorate(p_CToyota);
+    p_CAuto->Decorate(p_CNavi);
+    p_CAuto->showInfo();
+
+    Car* p_CVolo = new VOLOVL("Volovo");
+    Navigator* p_CNa2 = new Navigator();
+    SpeechControl* p_CSpeech = new SpeechControl();
+    p_CNa2->Decorate(p_CVolo);
+    p_CSpeech->Decorate(p_CNa2);
+    p_CSpeech->showInfo();
+
+    return 0;
+}
